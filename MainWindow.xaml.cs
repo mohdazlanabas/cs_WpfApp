5b15806b8e8b67e10b981edae51b92d92ddf9b48
Azlan Abas <mohdazlan.abas.com>
2022-05-07 14:42:12 +0800

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace cs_WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+
+        private void ButtonClearName_Click(object sender, RoutedEventArgs e)
+        {
+            lstNames.Items.Clear();
+            txtName.Clear();
+        }
+
+    }
+}
